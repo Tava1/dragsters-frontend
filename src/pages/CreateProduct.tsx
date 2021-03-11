d1a2a5ce85a003506f42cc1615e65c6d0e560ab5
Gustavo <gusta99sant@gmail.com>
2021-03-11 20:53:41 -0300

    chore: Connect to API to create new a product

--- src/pages/CreateProduct.tsx
+++ src/pages/CreateProduct.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router'
+
 import Link from 'next/link';
 
 import Navigation from '../components/Navigation';
@@ -7,8 +10,81 @@ import TextArea from '../components/TextArea';
 import Upload from '../components/Upload';
 
 import styles from '../styles/pages/CreateProduct.module.scss'
+import api from '../services/api';
 
 export default function CreateProduct() {
+  const router = useRouter();
+
+  const [productId, setProductId] = useState('');
+  const [productName, setProductName] = useState('');
+  const [productFullname, setProductFullname] = useState('');
+  const [brand, setBrand] = useState('');
+  const [description, setDescription] = useState('');
+  const [supply, setSupply] = useState();
+  const [price, setPrice] = useState();
+  const [isActive, setIsActive] = useState(true);
+
+  const [uploadedFiles, setUploadedFIles] = useState([]);
+
+  async function createNewProduct(e) {
+    e.preventDefault();
+
+    const showcase = new FormData();
+
+    uploadedFiles.map(item => {
+      showcase.append('showcase', item)
+    })
+
+    const data = {
+      product_name: productName,
+      product_fullname: productFullname,
+      brand,
+      description,
+      supply,
+      price,
+      status: isActive,
+      stars: 0,
+    }
+
+    try {
+      await api.post('/products', data).then((response) => {
+        console.log(response.data)
+        setProductId(response.data.product_id);
+      });
+
+      // await api.post(`/products/images/${productId}`, showcase, {
+      //   headers: {
+      //     'content-type': 'multipart/form-data'
+      //   }
+      // }).then((response) => {
+      //   console.log(response.data)
+      // })
+
+      router.push('/ListProducts');
+
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+
+  function setStatusTrue(e) {
+    e.preventDefault();
+    console.log(isActive)
+
+    if (isActive === true) {
+      return
+    }
+    setIsActive(true);
+  }
+
+  function setStatusFalse(e) {
+    e.preventDefault();
+    console.log(isActive)
+    if (isActive === false) {
+      return
+    }
+    setIsActive(false);
+  }
 
   return (
     <>
@@ -21,6 +97,7 @@ export default function CreateProduct() {
               <h2>Novo Produtos</h2>
               <p>Reúna as informações necessárias e cadastre um novo produto.</p>
             </div>
+            <Link href="/ListProducts">Lista de produtos</Link>
           </header>
 
           <main>
@@ -31,46 +108,76 @@ export default function CreateProduct() {
                   type="text"
                   title="Titulo"
                   id="product_name"
+                  value={productName}
+                  onChange={e => setProductName(e.target.value)}
                 />
 
                 <Input
                   type="text"
                   title="Titulo completo"
                   id="product_fullname"
+                  value={productFullname}
+                  onChange={e => setProductFullname(e.target.value)}
                 />
 
                 <Input
                   type="text"
                   title="Marca"
                   id="product_brand"
+                  value={brand}
+                  onChange={e => setBrand(e.target.value)}
                 />
               </div>
 
-              <TextArea title="Descrição" />
+              <TextArea
+                title="Descrição"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+              />
 
               <div className={styles.inputGroup}>
                 <Input
                   type="number"
                   title="Estoque"
                   id="supply"
+                  value={supply}
+                  onChange={e => setSupply(e.target.value)}
                 />
 
                 <Input
                   type="number"
                   title="Preço"
                   id="price"
+                  value={price}
+                  onChange={e => setPrice(e.target.value)}
                 />
 
                 <Input
                   type="number"
                   title="Avaliação"
                   id="stars"
+                  min="0"
+                  max="5"
+                  disabled
                 />
 
                 <div className={styles.isActive}>
                   <label htmlFor="">Ativo/Inativo</label>
                   {
-                    true ? <button className={styles.active}>ATIVO</button> : <button className={styles.inactive}>INATIVO</button>
+                    isActive ?
+                      <button
+                        className={styles.active}
+                        onClick={setStatusFalse}
+                      >
+                        ATIVO
+                    </button>
+                      :
+                      <button
+                        className={styles.inactive}
+                        onClick={setStatusTrue}
+                      >
+                        INATIVO
+                    </button>
                   }
                 </div>
               </div>
@@ -78,8 +185,12 @@ export default function CreateProduct() {
               <Upload />
 
               <div className={styles.actions}>
-                <Link href="/">Cancelar</Link>
-                <Button title="Salvar" type="submit" />
+                <Link href="/ListProducts">Cancelar</Link>
+                <Button
+                  title="Salvar"
+                  type="submit"
+                  onClick={createNewProduct}
+                />
               </div>
             </form>
           </main>
