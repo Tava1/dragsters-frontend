7b2b185a392cd2c101018ee8f4dd60f9916f3c3d
Gustavo <gusta99sant@gmail.com>
2021-03-11 05:00:04 -0300

    chore: Create Dropzone component

---
+++ src/components/Upload.tsx
@@ -0,0 +1,26 @@
+import { useDropzone } from 'react-dropzone';
+
+import styles from '../styles/components/Upload.module.scss';
+
+export default function Upload(props) {
+
+  const { acceptedFiles, fileRejections, getRootProps, getInputProps } = useDropzone({ accept: 'image/jpeg, image/png' });
+
+  const files = acceptedFiles.map(file => (
+    <p>{file.name}</p>
+  ));
+
+  return (
+    <section className={styles.container}>
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        <p>Click ou arraste para realizar o upload das imagens do produto.</p>
+      </div>
+      <h4>Imagens:</h4>
+      {files}
+      Quantidade: {files.length}
+      <aside>
+      </aside>
+    </section>
+  );
+}
