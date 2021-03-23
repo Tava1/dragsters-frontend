e89ea0e17ceda62c207c750362b05f42e8636f97
Gustavo <gusta99sant@gmail.com>
2021-03-23 01:36:39 -0300

    style: Using Product Card component, styling page

--- src/pages/products/ShowProducts.tsx
+++ src/pages/products/ShowProducts.tsx
@@ -1,26 +1,35 @@
 import styles from "../../styles/pages/ShowProducts.module.scss"
 import ProductCard from "../../components/modules/ProductCard"
 import Footer from "../../components/modules/Footer"
+import Header from "../../components/modules/Header"
 
-export default function ShowProducts(){
+export default function ShowProducts() {
   return (
     <>
+      <Header />
+
       <div className={styles.hero}>
-        <img src="/assets/" alt=""/>
-        <img src="#" alt=""/>
+        <div className={styles.banner}></div>
+        <div className={styles.logo}>
+          <img src="/assets/images/vossen-logo.png" alt="Vossen" />
+        </div>
       </div>
-      <div className={styles.productsList}>
-        <Footer />
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
       </div>
+
+      <Footer />
+
+
     </>
   )
 }
