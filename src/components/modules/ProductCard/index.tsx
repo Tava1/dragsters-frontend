f46515127be0744d5dbfd12c1002143d9d1de1ea
Gustavo <gusta99sant@gmail.com>
2021-03-23 01:35:13 -0300

    chore: Create Product Card component

--- src/components/modules/ProductCard/index.tsx
+++ src/components/modules/ProductCard/index.tsx
@@ -1,22 +1,21 @@
 import styles from "./styles.module.scss"
-export default function ProductCard(){
+
+export default function ProductCard() {
   return (
-<div className={styles.wheelDetails}>
-  <div className={styles.wheelImg}>
-    <img src="" alt="" />
-  </div>
-  <div className={styles.context}>
-    <div>
-      <span></span>
-      <h2></h2>
+    <div className={styles.card}>
+      <div className={styles.showcase}>
+        <img src="/assets/images/roda-rotiform.png" alt="Roda" />
+      </div>
+      <div className={styles.context}>
+        <div className={styles.header}>
+          <span>LHR</span>
+          <h2>LHR-F</h2>
+        </div>
+        <div className={styles.footer}>
+          <span><strong>R$ 2.000,00</strong>/CADA</span>
+          <h2>ROTIFORM</h2>
+        </div>
+      </div>
     </div>
-    <div>
-      <span>
-        <strong></strong>
-      </span>
-      <h2></h2>
-    </div>
-  </div>
-</div>
   );
 }
