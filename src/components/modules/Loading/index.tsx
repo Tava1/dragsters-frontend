6e40805a82bc9c68b2dba1afca0484429bb663e6
Victor Pedrasani <victor_steam@live.com>
2021-03-24 05:51:43 -0300

    loading animation

---
+++ src/components/modules/Loading/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss"
+
+export default function Loading(){
+  return(
+    <div className={styles.container}>
+      <div className={styles.loadIcon}>
+        <img src="/assets/images/load-wheel.svg" alt="Roda de l"/>
+      </div>
+    </div>
+  )
+}
